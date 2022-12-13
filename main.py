31690e3d74aff761fc2eecbb70f9e82edadac9ae
Sennur Aldemir <snnrldmr@gmail.com>
2022-12-13 09:50:18 +0300

    ilk commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#Python: Bir Yazı İçerisindeki Türkçe Karakterleri İngilizce Karakterlere Çevirme İşlemi
+önceki_karakterler =      ["ş",
+                          "ç",
+                          "ö",
+                          "ğ",
+                          "ü",
+                          "ı",
+                          "Ş",
+                          "Ç",
+                          "Ö",
+                          "Ğ",
+                          "Ü",
+                          "İ"]
+
+sonraki_karakterler =   ["s",
+                         "c",
+                         "o",
+                         "g",
+                         "u",
+                         "i",
+                         "S",
+                         "C",
+                         "O",
+                         "G",
+                         "U",
+                         "I"]
+
+metin = input("Çeviri yapılacak metni giriniz  :   ")
+for i in range(12):
+    metin=metin.replace(önceki_karakterler[i],sonraki_karakterler[i])
+print(metin)
