0c01b45d44d4b3e33fbeeea0da1a964f89ec53f5
ZehraSahaplioglu <99864727+ZehraSahaplioglu@users.noreply.github.com>
2022-12-15 13:14:57 +0300

    Update main.py
    zehra ikinci commit

--- main.py
+++ main.py
@@ -30,5 +30,14 @@ for i in range(12):
     metin=metin.replace(önceki_karakterler[i],sonraki_karakterler[i])
 print(metin)
 
+aranan_harf=input("Lütfen aradığınız harfi giriniz:\t")
+adet=0
+for karakter in metin:
+    if karakter==aranan_harf:
+        adet+=1
+print(adet)
+
+
+
 
 
